27aa0544338a6403531a0cf624a07c084e1c0144
aniket19233-maker <73880538+aniket19233-maker@users.noreply.github.com>
2022-12-21 01:36:33 +0530

    Update train_with_mixmatch.py

--- train_with_mixmatch.py
+++ train_with_mixmatch.py
@@ -4,6 +4,7 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 from load_dataset import *
+import copy
 import transform
 from wideresnet import WideResNet, CNN, WNet
 
@@ -15,6 +16,59 @@ import os
 
 import imgaug.augmenters as iaa
 
+# def get_augmenter():
+#     seq = iaa.Sequential([
+#         iaa.Crop(px=(0, 16)),
+#         iaa.Fliplr(0.5),
+#         iaa.GaussianBlur(sigma=(0, 3.0))
+#     ])
+#     def augment(images):
+#         # print(images.shape)
+#         return seq.augment(images = images.cpu().numpy())
+#     return augment
+
+# def sharpen(x, T):
+#     temp = x**(1/T)
+#     return temp / temp.sum(axis=1, keepdims=True)
+
+# def mixup(x1, x2, y1, y2, alpha):
+#     beta = torch.distributions.Beta(alpha, 1-alpha).sample()
+#     print(beta)
+#     x = torch.mul(x1, beta) + torch.mul(x2, (1 - beta))
+#     y = torch.mul(y1, beta) + torch.mul((1 - beta), y2)
+#     return x, y
+
+
+# def mixmatch(x, y, u, model, augment_fn, T=0.5, K=2, alpha=0.75):
+#     # torch.to('cuda')
+#     print("Y_SHAPE: ", y.shape)
+#     xb = torch.tensor(augment_fn(x)).to('cuda')
+#     print("xb_SHAPE: ", xb.shape)
+#     ub = [torch.tensor(augment_fn(u)).to('cuda') for _ in range(K)]
+#     print("ub_len_SHAPE: ", len(ub))
+#     print("Y_SHAPE: ",y.shape)
+#     print("model_SHAPE: ",model(ub[0]).shape)
+#     qb = sharpen(sum(map(lambda i: model(i), ub)) / K, T)
+#     print("qb_SHAPE: ", qb.shape)
+#     QB = torch.tensor([qb[i].argmax() for i in range(qb.shape[0])]).to('cuda') 
+#     # print(y.unique())
+#     # print(qb)
+#     print(QB.shape)
+#     Ux = torch.cat(ub, axis = 0)
+#     print("ux_SHAPE: ", Ux.shape)
+#     Uy = torch.cat([QB for _ in range(K)], axis=0)
+#     print("UY_SHAPE: ", Uy.shape)
+#     t = torch.arange(len(xb) + len(Ux))
+#     indices = t[torch.randperm(t.shape[0])]
+#     Wx = torch.cat([Ux, xb], axis=0)[indices]
+#     Wy = torch.cat([QB, y], axis=0)[indices]
+#     print(Wx.shape, Wy.shape)
+#     print(Ux.shape, Uy.shape)
+#     X, p = mixup(xb, Wx[:len(xb)], y, Wy[:len(xb)], alpha)
+#     U, q = mixup(Ux, Wx[len(xb):], Uy, Wy[len(xb):], alpha)
+#     return X, U, p, q
+
+
 def get_augmenter():
     seq = iaa.Sequential([
         iaa.Crop(px=(0, 16)),
@@ -22,34 +76,105 @@ def get_augmenter():
         iaa.GaussianBlur(sigma=(0, 3.0))
     ])
     def augment(images):
-        return seq.augment(images.transpose(1, 2, 0)).transpose(1, 2, 0)
+        return seq.augment(images=images)
     return augment
-
+    
 def sharpen(x, T):
     temp = x**(1/T)
     return temp / temp.sum(axis=1, keepdims=True)
+     
 
-def mixup(x1, x2, y1, y2, alpha):
-    beta = torch.distributions.Beta(alpha, -alpha)
+def mixup(x1, x2, alpha):
+    # print(x1.shape, x2.shape)
+    # print(y1.shape, y2.shape)
+    beta = np.random.beta(alpha, alpha)
+    beta = max(beta, 1 - beta)
+    # print(x1.shape, x2.shape)
     x = beta * x1 + (1 - beta) * x2
-    y = beta * y1 + (1 - beta) * y2
-    return x, y
+    return x
+
+# def find_labels(p, y_):
+#   # print("HEY")
+#   y = np.zeros(p.shape, dtype = 'int')
+#   for i in range(p.shape[0]):
+#     dist = math.inf
+#     cls = 0
+#     for k in y_.keys():
+#       d = abs(y_[k]-p[i])**2
+#       if(d <= dist):
+#         dist = d
+#         cls = k
+#         # break
+#     p[i] = cls
+#   return y
 
 
-def mixmatch(x, y, u, model, augment_fn, T=0.5, K=2, alpha=0.75):
-    xb = torch.cat(augment_fn(x))
-    ub = [augment_fn(u) for _ in range(K)]
-    qb = sharpen(sum(map(lambda i: model(i), ub)) / K, T)
-    Ux = torch.cat(ub, axis=0)
-    Uy = torch.cat([qb for _ in range(K)], axis=0)
-    t = torch.arange(len(xb) + len(Ux))
-    indices = t[torch.randperm(t.shape[0])]
-    Wx = torch.cat([Ux, xb], axis=0)[indices]
-    Wy = torch.cat([qb, y], axis=0)[indices]
-    X, p = mixup(xb, Wx[:len(xb)], y, Wy[:len(xb)], alpha)
-    U, q = mixup(Ux, Wx[len(xb):], Uy, Wy[len(xb):], alpha)
-    return X, U, p, q
-
+def mixmatch(x, y, u, u__, model, augment_fn, T=0.5, K=2, alpha=0.75):
+    # print(y.shape)
+    y_ = np.zeros(y.shape, dtype ='int')
+    for i in range(y_.shape[0]):
+      y_[i] = y[i]
+    # print(u.shape)
+    ub = [augment_fn(u) for _ in range(K)] 
+    ul = np.concatenate([u__ for _ in range(K)], axis=0) #unlabeled 
+    u_hat = np.concatenate(ub, axis=0) # unlabeled and augmented
+    xb = augment_fn(x) #augmented labeled set
+    x____ = np.concatenate([u_hat, xb], axis =0) #150
+    y____ = np.concatenate([ul, y], axis = 0) # 150
+    indices = np.arange(x____.shape[0])
+    # np.random.shuffle(indices)
+    x____ = x____[indices]
+    y____ = y____[indices]
+    x_x = mixup(xb, x____[:y.shape[0]], alpha)
+    u_u = mixup(u_hat, x____[y.shape[0]:], alpha)
+    # print(x_x.shape)
+    # print(u_u.shape)
+    l_images, u_images = x_x, u_u
+    l_labels, u_labels = y____[:y.shape[0]], y____[y.shape[0]:]
+    # print(l_labels.shape, u_labels.shape)
+    return torch.tensor(x).to('cuda'), torch.tensor(u).to('cuda'), torch.tensor(y).to('cuda'), torch.tensor(u__).to('cuda')
+    # unique, frequency = np.unique(y_, return_counts = True) 
+    # # y_mp = {}
+    # y = np.zeros((len(y_), len(unique)))
+    # # for i in range(len(unique)):
+    # #   y_mp[unique[i]] = frequency[i]/sum(frequency)
+    # # for i in range(y.shape[0]):
+    # #   for c in range(len(unique)):
+    # #     y[i][c] = y_mp[y_[i]] * (y_mp[y_[i]]*y_mp[c])
+    # # y = np.array([y_mp[i]/frequency[i] for i in y_])
+    # qb = sharpen(sum(map(lambda i: model(torch.tensor(i).to('cuda')), ub)) / K, T)
+    # QB = qb.cpu().detach().numpy()
+    # print(QB.shape)
+    # # QB = np.array([i.argmax() for i in QB])
+    # # QB = np.concatenate((QB, QB), axis = 0)
+    # # print(QB.shape)
+    # Ux = np.concatenate(ub, axis = 0)
+    # Uy = np.concatenate([QB for _ in range(K)], axis=0)
+    # # print(len(xb) + len(Ux))
+    # indices = np.arange(len(xb) + len(Ux))
+    # np.random.shuffle(indices)
+    # Wx = np.concatenate([Ux, xb], axis=0)[indices]
+    # Wy = np.concatenate([Uy, y], axis=0)[indices]
+    # # print(Uy.shape, Ux.shape, Wx.shape, Wy.shape)
+    # # print(np.random.shuffle(np.arange(len(xb) + len(Ux))))
+    # X= mixup(xb, Wx[:len(xb)], y, Wy[:len(xb)], alpha)
+    # U= mixup(Ux, Wx[len(xb):], Uy, Wy[len(xb):], alpha)
+    # # def compute_labels(y__):
+    # #   return [y__[i].argmax() for i in range(y__.shape[0])]
+    # print("XL", xl.shape)
+    # y_hat = np.concatenate([xl, y_], axis=0)[indices]
+    # py = y_hat[:len(xb)]
+    # # # print(py)
+    # qy = y_hat[len(xb):]
+    # # p, q = compute_labels(p), compute_labels(q)
+    # # p = find_labels(p, y_mp)
+    # # q = find_labels(q, y_mp)
+    # print(X.shape)
+    # print(U.shape)
+    # # print(py.shape)
+    # # print(qy.shape)
+    # # del p, q, y_hat, xb
+    # return torch.tensor(X).to('cuda'), torch.tensor(U).to('cuda'), torch.tensor(py).to('cuda'), torch.tensor(qy).to('cuda')
 
 
 
@@ -126,6 +251,7 @@ def bi_train(model, label_loader, unlabeled_loader, val_loader, test_loader, opt
         iteration += 1
         l_images, l_labels, _ = l_data
         u_images, u_labels, idx = u_data
+        # print(type(l_images))
         if args.dataset == 'MNIST':
             l_images = l_images.unsqueeze(1)
             u_images = u_images.unsqueeze(1)
@@ -135,9 +261,12 @@ def bi_train(model, label_loader, unlabeled_loader, val_loader, test_loader, opt
         model.train()
         meta_net = build_model()
         meta_net.load_state_dict(model.state_dict())
-
-#         l_images, u_images, l_labels, u_labels = mixmatch(torch.cat(l_images), torch.cat(l_labels), torch.cat(u_images), meta_net,get_augmenter())
-
+        # print(type(l_labels))
+        # print(l_labels.shape)
+        l_images, u_images, l_labels, u_labels = mixmatch(l_images.cpu().numpy(), l_labels.cpu().numpy(), u_images.cpu().numpy(), u_labels.cpu().numpy(), CNN(n_out=args.n_class).to(device),get_augmenter())
+        # print(type(l_labels))
+        # print(type(l_images))
+        # print(type(l_labels))
         # cat labeled and unlabeled data
         labels = torch.cat([l_labels, u_labels], 0)
         labels[-len(u_labels):] = -1 #unlabeled mask
@@ -161,7 +290,7 @@ def bi_train(model, label_loader, unlabeled_loader, val_loader, test_loader, opt
             loss_hat = cls_loss + coef * (torch.sum(cost_w * weight) / norm + ssl_loss[:len(l_labels)].mean())
         else:
             loss_hat = cls_loss + coef * (torch.sum(cost_w * weight) + ssl_loss[:len(l_labels)].mean())
-
+        # meta_net.to(device)
         meta_net.zero_grad()
         grads = torch.autograd.grad(loss_hat, (meta_net.params()), create_graph=True)
         meta_net.update_params(lr_inner=args.meta_lr, source_params=grads)
@@ -188,7 +317,6 @@ def bi_train(model, label_loader, unlabeled_loader, val_loader, test_loader, opt
             loss = cls_loss + coef * (torch.sum(cost_w * weight) / norm + ssl_loss[:len(l_labels)].mean())
         else:
             loss = cls_loss + coef * (torch.sum(cost_w * weight) + ssl_loss[:len(l_labels)].mean())
-
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -211,7 +339,7 @@ def bi_train(model, label_loader, unlabeled_loader, val_loader, test_loader, opt
     print("Last Model Accuracy: {}".format(test(model, test_loader)))
     print("Test Accuracy: {}".format(test_acc))
 
-
+args.meta_lr = 0.005
 def test(model, test_loader):
     with torch.no_grad():
         model.eval()
